57dea70b255147397b28427c2e09bb988851f20a
Ayo Moses <ayomoses111@gmail.com>
2023-04-28 18:17:17 +0100

    Added test for the fizzbuzz method to check if it returns the expected value

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,8 +3,14 @@ describe 'Solver.fizzbuzz' do
   context 'Runs multiple tests for a solver instance' do
     it 'tests fizzbuz method receives the values appropiately' do
       solver = Solver.new
-      expect(solver).to receive(:fizzbuz).with(5)
+      expect(solver).to receive(:fizzbuzz).with(5)
       solver.fizzbuzz(5)
+    end
+
+    it 'tests fizzbuzz method returns fizzbuzz when the number is divisible by both 3 and 5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
     end
   end
 end
